0afae70d099da26b96284711e98e9f49652adbf3
semmolino <simon.messina@gmail.com>
2026-05-17 00:23:56 +0200

    Fix: Message component uses text prop, not children

--- frontend-react/src/pages/AdminPage.tsx
+++ frontend-react/src/pages/AdminPage.tsx
@@ -833,7 +833,7 @@ function MonatsabschlussSection() {
       </div>
 
       {runMsg && (
-        <Message type={runMsg.type} style={{ marginTop: 8 }}>{runMsg.text}</Message>
+        <Message type={runMsg.type} text={runMsg.text} style={{ marginTop: 8 }} />
       )}
     </div>
   )
